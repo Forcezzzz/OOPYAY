e98eb07f6d49274edc769738daebe7d5bfc09f88
Forcezzzz <phattiyaphanjongkulsthitchai@gmail.com>
2025-10-03 10:54:27 +0700

    Add files via upload

---
+++ shop/Item.java
@@ -0,0 +1,12 @@
+package shop;
+public class Item {
+	String name;
+	double price;
+	int quantity;
+	
+	public Item(String name, double price , int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+}
