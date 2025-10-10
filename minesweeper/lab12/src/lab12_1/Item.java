ccf68ebe08e905846ef9e67d10194dc2465d9c90
Forcezzzz <phattiyaphanjongkulsthitchai@gmail.com>
2025-10-10 10:53:52 +0700

    Add files via upload

---
+++ minesweeper/lab12/src/lab12_1/Item.java
@@ -0,0 +1,12 @@
+package lab12_1;
+public class Item {
+	String name;
+	double price;
+	int quantity;
+	
+	public Item(String name, double price , int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+}
